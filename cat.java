bcb2423ca11215ac429a27b51ad6338632f551c2
Yu Koga <ukoooga@gmail.com>
2017-06-15 18:23:22 +0900

    cat w/o arg partially done

--- cat.java
+++ cat.java
@@ -1,11 +1,14 @@
+import java.awt.event.KeyListener;
 import java.io.*;
+import java.util.Scanner;
 
 /**
  * Created by koga on 2017/06/13.
  */
 public class cat {
     public static void main(String[] args) {
-        File file = new File(args[0]);
+        if (args.length != 0) {
+            File file = new File(args[0]);
         try {
             FileReader r = new FileReader(file);
             BufferedReader br = new BufferedReader(r);
@@ -17,6 +20,14 @@ public class cat {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+        } else {
+            //addKeyListener
+            //do {
+                Scanner sc = new Scanner(System.in);
+                String stdin = sc.nextLine();
+                System.out.println(stdin);
+            //} while (keyPressed == false);
         }
     }
 }
