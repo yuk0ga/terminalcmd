ffec04195218b761e4c2d8a377e0699eab98b022
Yu Koga <ukoooga@gmail.com>
2017-06-22 17:17:00 +0900

    initial commit

--- ls.java
+++ ls.java
@@ -12,7 +12,21 @@ public class ls {
         for (File file : files) {
             System.out.println(file.getName());         //getName gets only the file name. Without this, the whole path will be listed.
         }
-        } else {
+        } else if ((args.length == 1) && args[0].contains("a") && args[0].contains("-")) {     //might not need "args.length == 1"
+            String workingDir = System.getProperty("user.dir");
+            File dir = new File(workingDir);
+            File[] files = dir.listFiles();
+            for (File file : files) {
+                System.out.println(file.getName());
+            }
+        } else if ((args.length == 1) && args[0].contains("l") && args[0].contains("-")) {
+            System.out.printf("This option is currently unavailable");
+        } else if ((args.length == 1) && args[0].contains("t") && args[0].contains("-")) {
+            System.out.printf("This option is currently unavailable");
+        } else if ((args.length == 1) && args[0].contains("r") && args[0].contains("-")) {
+            System.out.printf("This option is currently unavailable");
+        }
+        else {
             String calledDir = System.getProperty("user.dir")+ "/" + args[0];
             File argdir = new File(calledDir);
             File[] argfiles = argdir.listFiles();
