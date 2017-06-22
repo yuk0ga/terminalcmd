d90426153afde36459066d39ddd6b34212e7882a
Yu Koga <ukoooga@gmail.com>
2017-06-22 12:39:18 +0900

    cat w/o arg done

--- cat.java
+++ cat.java
@@ -1,26 +1,13 @@
-import java.applet.Applet;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.io.*;
 import java.util.Scanner;
 
-import static com.sun.java.accessibility.util.AWTEventMonitor.addKeyListener;
 
 /**
  * Created by koga on 2017/06/13.
  */
 
-/*
-abstract class Exit extends Applet implements KeyListener {
-    public void init() {
-        addKeyListener(this);
-    }
-    public void keyPressed(KeyEvent e) {
-    }
-}
-*/
-public class cat {
-    public static void main(String[] args) {
+public class cat{
+    public static void main(String[] args) throws IOException {
         if (args.length != 0) {
             File file = new File(args[0]);
         try {
@@ -36,16 +23,11 @@ public class cat {
             e.printStackTrace();
         }
         } else {
-            int x = 1;
-            while(x < 10) {
                 Scanner sc = new Scanner(System.in);
-                String stdin = sc.nextLine();
-                System.out.println(stdin);
-                x++;
-                //if (e.getModifiers() == KeyEvent.CTRL_MASK && e.getKeyCode() == KeyEvent.VK_D) {
-                //    break;
-                //}
-            }
+                while (sc.hasNext()) {
+                    String stdin = sc.nextLine();
+                    System.out.println(stdin);
+                }
         }
     }
 }
